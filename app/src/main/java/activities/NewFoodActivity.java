ff4fc2c16ff5736031ebdb4c5ebb976a2ee11a4c
Shogo Tsuneto <shogotsuneto@gmail.com>
2026-03-27 20:37:36 -0700

    Add NewFoodActivity image preview

--- app/src/main/java/activities/NewFoodActivity.java
+++ app/src/main/java/activities/NewFoodActivity.java
@@ -1,9 +1,11 @@
 package activities;
 
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.activity.result.ActivityResultLauncher;
@@ -16,7 +18,10 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.foodshare.R;
 
+import utils.ImageServer;
+
 public class NewFoodActivity extends AppCompatActivity {
+    ImageView imgUploadCandidate;
     ActivityResultLauncher<PickVisualMediaRequest> pickMedia;
     Uri photoUri;
 
@@ -30,10 +35,26 @@ public class NewFoodActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        imgUploadCandidate = findViewById(R.id.imgUploadPreview);
+
         pickMedia =
                 registerForActivityResult(new ActivityResultContracts.PickVisualMedia(), uri -> {
                     if (uri != null) {
                         photoUri = uri;
+                        ImageServer imgServer = new ImageServer(this);
+                        Bitmap bitmap = imgServer.loadImage(uri);
+                        if (bitmap == null) {
+                            Log.d("PhotoPicker", "Invalid media selected");
+                            photoUri = null;
+
+                            // clear image view
+                            // reference: https://stackoverflow.com/a/8243184
+                            imgUploadCandidate.setImageResource(0);
+                            return;
+                        }
+
+                        // valid image
+                        imgUploadCandidate.setImageBitmap(bitmap);
                     } else {
                         Log.d("PhotoPicker", "No media selected");
                     }
