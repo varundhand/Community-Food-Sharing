582a3aa90c565537952e5a96cff07eefa356377c
Shogo Tsuneto <shogotsuneto@gmail.com>
2026-03-30 21:26:40 -0700

    Display Food image and Food Name (Recipient detail)

--- app/src/main/java/activities/RecipientFoodItemActivity.java
+++ app/src/main/java/activities/RecipientFoodItemActivity.java
@@ -1,6 +1,12 @@
 package activities;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,7 +16,14 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.foodshare.R;
 
+import database.DatabaseHelper;
+import models.FoodItem;
+import utils.ImageServer;
+
 public class RecipientFoodItemActivity extends AppCompatActivity {
+    FoodItem item;
+    ImageView imgFoodItem;
+    TextView txtFoodName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +35,40 @@ public class RecipientFoodItemActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            int foodItemId = extras.getInt("FoodItemId");
+            DatabaseHelper dbHelper = new DatabaseHelper(this);
+            item = dbHelper.getFoodItem(foodItemId);
+        }
+
+        if (item == null) {
+            // Go back to donor home if the foodId is invalid
+            Toast.makeText(this, "Invalid Food", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(RecipientFoodItemActivity.this, DonorHomeActivity.class);
+            startActivity(intent);
+            return;
+        }
+
+        imgFoodItem = findViewById(R.id.imgFoodItem);
+        txtFoodName = findViewById(R.id.txtFoodName);
+        String imgFilename = item.getImageKey();
+
+        if (imgFilename != null && !imgFilename.isEmpty()) {
+            setPreviewPhoto(imgFilename);
+        }
+
+        txtFoodName.setText(item.getName());
+    }
+
+    private void setPreviewPhoto(String filename) {
+        if (filename != null) {
+            ImageServer imgServer = new ImageServer(this);
+            Bitmap bitmap = imgServer.loadImage(filename);
+            imgFoodItem.setImageBitmap(bitmap);
+        } else {
+            Log.d("PhotoPicker", "No media selected");
+        }
     }
 }
